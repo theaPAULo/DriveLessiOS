3b231095670a0b9a1f1130668d2b3eb18d7584fb
Paul <psoni511@gmail.com>
2025-06-19 10:34:16 -0500

    Initial Commit

---
+++ DriveLess/DriveLessApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriveLessApp.swift
+//  DriveLess
+//
+//  Created by Paul Soni on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct DriveLessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DriveLessTests/DriveLessTests.swift
@@ -0,0 +1,17 @@
+//
+//  DriveLessTests.swift
+//  DriveLessTests
+//
+//  Created by Paul Soni on 6/19/25.
+//
+
+import Testing
+@testable import DriveLess
+
+struct DriveLessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
